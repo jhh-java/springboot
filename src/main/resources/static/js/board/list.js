781d0b1b3c6e3462c59e9d6fef7b21cac0b1eb38
jhh01015@gmail.com <jhh01015@gmail.com>
2023-04-13 15:54:02 +0900

    게시판 페이징 처리

--- src/main/resources/static/js/board/list.js
+++ src/main/resources/static/js/board/list.js
@@ -20,6 +20,9 @@ var main = {
 			$('#btnBack').prop('hidden', false);
 			$('#btnLogout').prop('hidden', true);
 		}
+		console.log('_pageIdx : ' + _pageIdx);
+		console.log('_listLength : ' + _listLength);
+		console.log('_currentIdx : ' + _currentIdx);
 	},
 	
 	event : function(){
@@ -31,6 +34,30 @@ var main = {
 			localStorage.removeItem('boardDetail');
 			localStorage.removeItem('user');
 			location.href='/';
+		});
+		$('#page>.pageNum').on('click', function (e) {
+		    if(e.currentTarget.text != _pageIdx){
+			    _listLength = Number(e.currentTarget.text)*10;
+			    _currentIdx = _listLength - 9;
+			    _pageIdx = Number(e.currentTarget.text);
+			    
+			    console.log('_pageIdx : ' + _pageIdx);
+				console.log('_listLength : ' + _listLength);
+				console.log('_currentIdx : ' + _currentIdx);
+			    
+			    console.log(e);
+			    console.log(_currentIdx, _listLength);
+			    console.log('click page num : '+e.currentTarget.text);
+			    for(var i = 0; i< $('#page>a').length; i++){
+					if($('#page>a')[i].style.color != ''){
+						$('#page>a')[i].style = '';
+					}
+				}
+				e.currentTarget.style.fontWeight = 'bold';
+				e.currentTarget.style.fontSize = '1.2rem';
+				e.currentTarget.style.color = 'black';
+				main.callData2(_currentIdx, _listLength);
+			}
 		});
 	},
 	
@@ -45,105 +72,42 @@ var main = {
                 //서버로부터 정상적으로 응답이 왔을 때 실행
                 console.log(data);
                 _cnt = data;
-                $('#cnt').text($('#cnt').text()+data);
+                $('#cnt').text('');
+                $('#cnt').text('게시글 수 : '+data);
                 var page = Math.ceil(data/10);
+                $('.pageNum').remove();
                 var a;
-                for(var i = 10; i>=1; i--){
+                var maxNo = _pageIdx+9; 
+                if(maxNo > page){
+					maxNo = page;
+					$('#next').prop('hidden', true);
+				}
+				if(_pageIdx >10){
+					$('#prev').prop('hidden', false);
+				}else{
+					maxNo = 10;
+					_pageIdx = 1;
+					$('#prev').prop('hidden', true);
+				}
+                for(var i = maxNo; i>=_pageIdx; i--){
 					a = document.createElement('a');
 					a.href = 'javascript:void(0);';
 					a.text = i;
-					if(_currentIdx == i){
+					a.className = 'pageNum';
+					if(_pageIdx == i){
 						a.style.fontWeight = 'bold';
 						a.style.fontSize = '1.2rem';
 						a.style.color = 'black';
 					}
-					a.onclick = function(obj){
-						if(_currentIdx == Number(obj.currentTarget.text)){
-							false;
-						}else{
-							obj.currentTarget.style.fontWeight = 'bold';
-							obj.currentTarget.style.fontSize = '1.2rem';
-							obj.currentTarget.style.color = 'black';
-							main.page(this);
-						}
-						if(String(_currentIdx).includes('7')){
-							main.redraw();
-						}
-					}
 					document.getElementById('page').children[0].after(a);
 				}
-                main.callData2(_pageIdx, _listLength);
+                main.callData2(_currentIdx, _listLength);
             },
             error: function(err) {
                 //서버로부터 응답이 정상적으로 처리되지 못햇을 때 실행
                 console.log(err);
             }
         });
-	},
-	
-	redraw(){
-		var test1 = _currentIdx-4;
-		var test2 = _currentIdx+6;
-		console.log(test1,test2);
-		$('#page').html('');
-		$('#page').append(
-			'<a id="prev" href="javascript:void(0);" onclick="main.prePage();">[이전]</a>'
-			+'<a id="next" href="javascript:void(0);" onclick="main.nextPage();">[다음]</a>');
-		for(var i = test2; i>=test1; i--){
-			a = document.createElement('a');
-			a.href = 'javascript:void(0);';
-			a.text = i;
-			if(_currentIdx == i){
-				a.style.fontWeight = 'bold';
-				a.style.fontSize = '1.2rem';
-				a.style.color = 'black';
-			}
-			a.onclick = function(obj){
-				if(_currentIdx == Number(obj.currentTarget.text)){
-					false;
-				}else{
-					obj.currentTarget.style.fontWeight = 'bold';
-					obj.currentTarget.style.fontSize = '1.2rem';
-					obj.currentTarget.style.color = 'black';
-					main.page(this);
-				}
-				if(String(_currentIdx).includes('7')){
-					main.redraw();
-				}
-			}
-			document.getElementById('page').children[0].after(a);
-		}
-	},
-	
-	prePage(){
-		console.log(_currentIdx);
-		if(_currentIdx <= 10){
-			false;
-		}else{
-			
-		}
-	},
-	
-	nextPage(){
-//		_currentIdx = 11;
-//		$('#page').html()
-		Math.ceil(_currentIdx/10)*10
-//		$('#prev').after()
-		var test1 = _currentIdx-4;
-		var test2 = _currentIdx+6;
-		console.log(test1,test2);
-		if(String(_currentIdx).includes('7')){
-			for(var i = test1; i<=test2; i++){
-				$('#page').html('');
-			}
-		}
-	},
-	
-	page(obj){
-		var listLength = Number(obj.text) * _listLength;
-		_currentIdx = Number(obj.text); 
-		var pageIdx = listLength-9;
-		main.callData2(pageIdx, listLength);
 	},
 	
 	callData2(pageIdx, listLength){
@@ -166,57 +130,79 @@ var main = {
 	
 	drawTable(data){
 		console.log(data);
+		$('#table').html('');
 		_listData = data;
 		var template;
 			template	+=	'<tr>';
 			template	+=		'<th>';
+			template	+=		'No';
+			template	+=		'</th>';
+			template	+=		'<th>';
+			template	+=		'ID';
+			template	+=		'</th>';
+			template	+=		'<th>';
 			template	+=		'제목';
-			template	+=		'</th>';
-			template	+=		'<th>';
-			template	+=		'내용';
-			template	+=		'</th>';
-			template	+=		'<th>';
-			template	+=		'게시일';
 			template	+=		'</th>';
 			template	+=		'<th>';
 			template	+=		'게시자';
 			template	+=		'</th>';
+			template	+=		'<th>';
+			template	+=		'게시일';
+			template	+=		'</th>';
 			template	+=	'/<tr>';
 		data.forEach(function(list){
-			console.log(list);
 			template	+=	'<tr>';
+			template	+=		'<td>';
+			template	+=			list.rownum;
+			template	+=		'</td>';
+			template	+=		'<td>';
+			template	+=			'<a href="/goDetail?id='+list.id+'">';
+			template	+=				list.id;
+			template	+=			'<a>';
+			template	+=		'</td>';
 			template	+=		'<td>';
 			template	+=			'<a href="/goDetail?id='+list.id+'">';
 			template	+=				list.title;
 			template	+=			'<a>';
 			template	+=		'</td>';
 			template	+=		'<td>';
-			template	+=			'<a href="/goDetail?id='+list.id+'">';
-			template	+=				list.detail;
-			template	+=			'<a>';
+			template	+=		list.modifyName;
 			template	+=		'</td>';
 			template	+=		'<td>';
 			template	+=		list.modifyDate.substr(0,10);
 			template	+=		'</td>';
-			template	+=		'<td>';
-			template	+=		list.modifyName;
-			template	+=		'</td>';
 			template	+=	'/<tr>';
 		});
 		$('#table').html(template);
-		for(var i=0; i< document.getElementById('page').children.length; i++){
-			if(_currentIdx != i){
-				document.getElementById('page').children[i].style = '';
-			}else{
-				if(String(_currentIdx).includes('7')){
-					
-				}
-				
-			}
-		}
-		if(_currentIdx > 10){
-			$('#prev').prop('hidden', false);
-		}
+		main.event();
+	},
+	
+	prePage(obj){
+		_currentIdx = _currentIdx-100;
+	    _listLength = _listLength -100;
+	    _pageIdx = Number(obj.nextElementSibling.text)-10;
+	    
+	    console.log('_pageIdx : ' + _pageIdx);
+		console.log('_listLength : ' + _listLength);
+		console.log('_currentIdx : ' + _currentIdx);
+		
+		console.log(_currentIdx, _listLength);
+		
+		main.callData();
+	},
+	
+	nextPage(obj){
+		_listLength = (Number(obj.previousElementSibling.text)+1)*10;
+	    _currentIdx = _listLength - 9;
+	    _pageIdx = Number(obj.previousElementSibling.text)+1;
+	    
+	    console.log('_pageIdx : ' + _pageIdx);
+		console.log('_listLength : ' + _listLength);
+		console.log('_currentIdx : ' + _currentIdx);
+	    
+		console.log(_currentIdx, _listLength);
+//		main.callData2(_currentIdx, _listLength);
+		main.callData();
 	},
 	
 	detail(id){
